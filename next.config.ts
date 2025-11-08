8646251d98d429d53f0c08bfecc991ed4e44376c
majeri03 <105841103622@student.unismuh.ac.id>
2025-11-08 12:30:23 +0800

    fixed image

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,16 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+       hostname: 'koperasi.kirinxe00.workers.dev',
+      },
+      {
+        protocol: 'https',
         hostname: 'pub-d1365018603a44b4a560230bd94b56fe.r2.dev',
+      },
+      // --- DAN JUGA YANG INI (dari error sebelumnya) ---
+      {
+        protocol: 'https',
+        hostname: 'koperasi-dokumen-pendaftaran.e20806dfd4fa71c26e01153f020a638f.r2.cloudflarestorage.com',
       },
     ],
   },
